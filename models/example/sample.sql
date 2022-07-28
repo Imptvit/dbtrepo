c4777b30a0ca1b19041d98c390d36c5f41cbbe12
Vithal Jadeppa Sampagar <vithal.sampagar@impetus.com>
2022-07-28 19:49:29 +0530

    File connections1

--- models/example/sample.sql
+++ models/example/sample.sql
@@ -1,5 +1,5 @@
 select *
-from {{ ref('I_SAP_ENT_LCTRTV_StagingLoad 2') }} as a
+from {{ ref('I_SAP_ENT_LCTRTV_StagingLoad2') }} as a
 join {{ref('I_SAP_ENT_LCTRTV_StagingLoad')}} as b
 on (a.id = b.id)
 where id = 1
