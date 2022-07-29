b2ff7034345c0b09e50c05ed5a39c653c1337cfd
Vithal Jadeppa Sampagar <vithal.sampagar@impetus.com>
2022-07-29 12:41:49 +0530

    new model

---
+++ models/example/LOCATION_TRAIT_VALUE_TMP.sql
@@ -0,0 +1,13 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='LOC_TRAIT_VAL_CD',
+    incremental_strategy='delete+insert'
+  )
+}}
+
+SELECT 
+ LOC_TRAIT_VAL_CD,
+ LOC_TRAIT_CD,
+ LOC_TRAIT_VAL_DESC
+ FROM {{ ref('LOCATION_TRAIT_VALUE_WRK_11') }};	
