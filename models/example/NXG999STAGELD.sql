d017ca17adfa286a154a58e4fa4438829659b4ce
Vithal Jadeppa Sampagar <vithal.sampagar@impetus.com>
2022-08-01 11:07:46 +0530

    new nxg files

---
+++ models/example/NXG999STAGELD.sql
@@ -0,0 +1,15 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='LOC_TRAIT_VAL_CD',
+    incremental_strategy='delete+insert'
+  )
+}}
+
+SELECT 
+ a.LOC_TRAIT_VAL_CD,
+ a.LOC_TRAIT_CD,
+ b.LOC_TRAIT_VAL_DESC
+ FROM {{ ref('LOCATION_TRAIT_VALUE_WRK_11') }} as a
+ JOIN {{ ref('NXG999STAGELD') }} as b
+ ON (a.LOC_TRAIT_VAL_CD = b.LOC_TRAIT_VAL_CD)	
