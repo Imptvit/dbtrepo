91ee3e6515b5495ca48300991fb6f34fc911ba43
Vithal Jadeppa Sampagar <vithal.sampagar@impetus.com>
2022-08-01 11:17:43 +0530

    new nxg files

--- models/example/NXGSTAGELD.sql
+++ models/example/NXGSTAGELD.sql
@@ -11,5 +11,5 @@ SELECT
  a.LOC_TRAIT_CD,
  b.LOC_TRAIT_VAL_DESC
  FROM {{ ref('LOCATION_TRAIT_VALUE_WRK_11') }} as a
- JOIN {{ ref('NXGSTAGELD') }} as b
+ JOIN {{ ref('NXGFILELD') }} as b
  ON (a.LOC_TRAIT_VAL_CD = b.LOC_TRAIT_VAL_CD)	
