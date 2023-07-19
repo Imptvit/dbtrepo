f151b65c15c17f113875a30f87d7b63593ad55ca
vithalimpetus@gmail.com <vithalimpetus@gmail.com>
2023-07-19 05:33:08 +0000

    change

--- models/example/test2.sql
+++ models/example/test2.sql
@@ -4,16 +4,14 @@
   )
 }}
 
-MERGE INTO test_1
+MERGE INTO {{ref('test_1')}}
 USING test_2
 ON (test_1.ID = test_2.id)
 WHEN NOT MATCHED THEN insert(test_1.id,test_1.col2) 
 values(test_2.id, test_2.COL2);
 
 update {{ref('test_1')}}
-SET test_1.COL2 ='Sam'
-WHEre test_1.COL2='aa';
+SET test_1.COL2 ='Jain'
+WHEre test_1.COL2='bb';
 
 create or replace table {{this}} as select * from {{ref('test_1')}};
-
-select * from {{this}}
